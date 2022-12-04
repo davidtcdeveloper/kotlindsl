200a2952226d497223638d944104ef95b0374c8d
David Tiago Conceição <davidtc.developer@gmail.com>
2022-12-04 18:31:34 +0000

    Adds first step

---
+++ sample-code/app/src/main/java/com/davidtiago/kotlindsl/samplecode/Step1.kt
@@ -0,0 +1,17 @@
+package com.davidtiago.kotlindsl.samplecode
+
+import okhttp3.*
+
+/*
+ * Request.Builder is not required anymore
+ */
+fun request(block: (Request.Builder) -> Unit): Request =
+        Request.Builder().also { builder -> block(builder) }.build()
+
+fun `using step1`() {
+    val request =request{ builder ->
+                    builder.url("https://publicobject.com/helloworld.txt")
+                    builder.addHeader("headerKey", "headerValue")
+                }
+    // ...
+}
