87cdc41be7cb600f032c21c94ea5a9afbebe57d4
David Tiago Conceição <davidtc.developer@gmail.com>
2022-12-06 18:34:15 -0300

    Adds step four

--- sample-code/app/src/main/java/com/davidtiago/kotlindsl/samplecode/StepFour.kt
+++ sample-code/app/src/main/java/com/davidtiago/kotlindsl/samplecode/StepFour.kt
@@ -1,47 +1,15 @@
+@file:JvmName("StepFiveKt")
+
 package com.davidtiago.kotlindsl.samplecode
 
-import okhttp3.MediaType
-import okhttp3.MediaType.Companion.toMediaType
-import okhttp3.OkHttpClient
-import okhttp3.RequestBody.Companion.toRequestBody
-import java.io.IOException
+import okhttp3.Request
 
-class UsingStepFour {
+fun requestWithReceiver(block: Request.Builder.() -> Unit): Request =
+    Request.Builder().also { builder -> builder.block() }.build()
 
-    private val client = OkHttpClient()
-
-    fun wrongCall() {
-
-        val JSON: MediaType = "application/json".toMediaType()
-        val body = "{}".toRequestBody(JSON)
-
-        // Wrong call: adds method GET and post body
-        client.call {
-            requestWithReceiver {
-                url("https://publicobject.com/helloworld.txt")
-                addHeader("headerKey", "headerValue")
-                method("Get", null)
-                post(body)
-            }
-        }
-    }
-
-    fun httpGet() {
-        client
-            .call {
-                requestWithReceiver {
-                    url("https://publicobject.com/helloworld.txt")
-                    addHeader("headerKey", "headerValue")
-                }
-            }
-            .use { response ->
-                if (!response.isSuccessful) throw IOException("Unexpected code $response")
-
-                for ((name, value) in response.headers) {
-                    println("$name: $value")
-                }
-
-                println(response.body?.string())
-            }
+fun afterStepFour() {
+    requestWithReceiver {
+        url("https://publicobject.com/helloworld.txt")
+        addHeader("headerKey", "headerValue")
     }
 }
