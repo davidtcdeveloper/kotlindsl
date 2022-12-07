30be0977baba94adbc44b7f641f63974c7ae4717
David Tiago Conceição <davidtc.developer@gmail.com>
2022-12-07 21:28:22 +0000

    Creating step five

--- sample-code/app/src/main/java/com/davidtiago/kotlindsl/samplecode/StepFive.kt
+++ sample-code/app/src/main/java/com/davidtiago/kotlindsl/samplecode/StepFive.kt
@@ -28,20 +28,39 @@ fun beforeStepFive() {
     }
 }
 
-class RequestBuildReceiver(val requestBuilder: Request.Builder)
+interface BuilderReceiverMethod {
+    val requestBuilder: Request.Builder
+}
 
-infix fun RequestBuildReceiver.get(url: String): GetBuildReceiver =
-    GetBuildReceiver(requestBuilder.url(url))
+internal fun BuilderReceiverMethod.buildRequest() = requestBuilder.build()
 
-class GetBuildReceiver(val requestBuilder: Request.Builder)
+class RequestBuildReceiver(override val requestBuilder: Request.Builder): BuilderReceiverMethod
 
-infix fun GetBuildReceiver.withHeaders(headers: () -> List<Pair<String, String>>): Request =
-    requestBuilder.headers(headers).build()
+fun RequestBuildReceiver.get(block: ()->String): GetBuildReceiver =
+    GetBuildReceiver(requestBuilder.url(block()))
 
-fun requestTyped() {
-    //TODO
+class GetBuildReceiver(override val requestBuilder: Request.Builder): BuilderReceiverMethod
+
+class FinalBuilderReceiver(override val requestBuilder: Request.Builder): BuilderReceiverMethod
+
+infix fun GetBuildReceiver.withHeaders(headers: () -> List<Pair<String, String>>): FinalBuilderReceiver =
+    FinalBuilderReceiver(requestBuilder.headers(headers))
+
+fun requestTyped(block: RequestBuildReceiver.() -> BuilderReceiverMethod) {
+    RequestBuildReceiver(
+        Request.Builder()
+    ).block()
+    .buildRequest()
 }
 
 fun afterStepFive() {
-    // TODO
+    requestTyped {
+        get {
+            "https://publicobject.com/helloworld.txt"
+        } withHeaders {
+            ("User-Agent" to "OkHttp Headers.java") +
+                    ("Accept" to "application/json; q=0.5") +
+                    ("Accept" to "application/vnd.github.v3+json")
+        }
+    }
 }
